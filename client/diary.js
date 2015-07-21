362288302e2bd5e473cfa8e2f7b6227e4f71eadc
yoonjib <yoonji_519@hotmail.com>
2015-07-20 22:35:52 -0700

    arranged the list and the title and body

--- client/diary.js
+++ client/diary.js
@@ -5,13 +5,14 @@ Template.diary.events({
 		var title= $('#diaryTitle').val();
 		var body= $('#diaryBody').val();
 		Meteor.call('submitPost',title,body, currentUserId);
+		//Lists.insert({title:title});
 	},
 
 	'click .logout': function(evt){
 		evt.preventDefault();
 		Meteor.logout();
 		Router.go('/');
-	}	
+	},
 
 });
 
@@ -19,3 +20,27 @@ Template.listDiarys.diarys = function(){
 	return Diarys.find({userId: Meteor.userId()});
 }
 
+/*Template.listDiarys.events({
+	'keyup .title': function(evt,t){
+		if(evt.which ===13){ //when you enter 
+			var title = t.find('.title').value;
+			Lists.insert({title:title}); //put the name in the list in database
+		}
+	}
+});*/
+
+//Whenever someone calls out a list, we want to return that file
+/*Template.listDiarys.lists=function(){
+	return Lists.find();
+}*/
+
+//If someone clicks the list, it returns the id of the list.
+Template.listitem.events({
+	'click .list': function(e,t){
+		Session.set('listid',this._id);
+	},
+
+	'click .removelist':function(e,t){
+		Diarys.remove({_id:this._id});
+	}
+})
