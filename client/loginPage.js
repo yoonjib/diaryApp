1803cd8747031c4dddbadc53bf1dc101c00cabbd
yoonjib <yoonji_519@hotmail.com>
2015-07-22 18:39:20 -0700

    combined login and register in one page, change UI for login panel

--- client/loginPage.js
+++ client/loginPage.js
@@ -20,11 +20,7 @@ Template.loginPage.events({
     return false;
   },
 
-  'click .js-register': function(){
-    Router.go('/register');
-  },
-
-  'click .js-fblogin' : function(e){
+  'click .social-login' : function(e){
    
     Meteor.loginWithFacebook({}, function (err) {
       if (err){
@@ -33,7 +29,26 @@ Template.loginPage.events({
       }
     });
     console.log("clicked");
-  }
+  },
+
+  'click .js-register' : function(e, t) {
+      e.preventDefault();
+      var email = t.find('#account-email').value
+        , password = t.find('#account-password').value;
+
+        // Trim and validate the input
+
+      Accounts.createUser({email: email, password : password}, function(err){
+          if (err) {
+            alert("Double check your email: email is empty/ already existed !");
+          } else {
+            Router.go('/');
+          }
+
+        });
+
+      return false;
+    }
 
   
 });
