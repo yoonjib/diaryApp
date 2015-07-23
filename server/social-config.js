c010ef8da10897b94b6dfdd187662638658b0705
yoonjib <yoonji_519@hotmail.com>
2015-07-22 17:14:45 -0700

    added not added files

---
+++ server/social-config.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '406671079537309',
+    secret: '6bfbf7f94e7dd385ae776c82d49f1e98'
+});
+
